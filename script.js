dd8d2f73eaf2b19b9afea4775d0ccea0061c2c36
Michel <mikescosta@outlook.com>
2022-11-03 19:59:22 -0300

    Criação do background

---
+++ script.js
@@ -0,0 +1,22 @@
+let canvas = document.getElementById("snake");
+let context =canvas.getContext("2d");
+let box = 32;
+let snake [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+function criarBG() {
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha() {
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+}
+
+criarBG();
